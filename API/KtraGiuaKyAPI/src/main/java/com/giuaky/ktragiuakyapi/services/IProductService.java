e8b669814dc1a60314e4e5134c27b305835cc1a8
dt256 <buiducthang13022004@gmail.com>
2025-03-21 10:45:24 +0700

    update

---
+++ API/KtraGiuaKyAPI/src/main/java/com/giuaky/ktragiuakyapi/services/IProductService.java
@@ -0,0 +1,13 @@
+package com.giuaky.ktragiuakyapi.services;
+
+import com.giuaky.ktragiuakyapi.entity.Product;
+
+import java.util.List;
+
+//22110429_VoNguyenXuanThinh
+public interface IProductService {
+    List<Product> getLatestProducts(int page, int limit);
+    int getTotalProducts();
+    List<Product> getProductsByCategoryId(Long categoryId, int page, int limit); // Thêm phương thức mới
+    int getTotalProductsByCategoryId(Long categoryId);
+}
--- API/KtraGiuaKyAPI/src/main/java/com/giuaky/ktragiuakyapi/services/Impl/ProductServiceImpl.java
+++ API/KtraGiuaKyAPI/src/main/java/com/giuaky/ktragiuakyapi/services/Impl/ProductServiceImpl.java
@@ -1,7 +1,7 @@
 package com.giuaky.ktragiuakyapi.services.Impl;
 
 import com.giuaky.ktragiuakyapi.entity.Product;
-import com.giuaky.ktragiuakyapi.repository.IProductService;
+import com.giuaky.ktragiuakyapi.services.IProductService;
 import com.giuaky.ktragiuakyapi.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -29,5 +29,15 @@ public class ProductServiceImpl implements IProductService {
     //22110429_VoNguyenXuanThinh
     public int getTotalProducts() {
         return (int) productRepository.count();
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryId(Long categoryId, int page, int limit) {
+        return List.of();
+    }
+
+    @Override
+    public int getTotalProductsByCategoryId(Long categoryId) {
+        return 0;
     }
 }
