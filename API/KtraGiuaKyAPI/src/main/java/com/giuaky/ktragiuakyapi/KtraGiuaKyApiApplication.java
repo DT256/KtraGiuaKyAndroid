907e8c0f36076dd4a3f0cd5a886a71b69af8fa1b
dt256 <buiducthang13022004@gmail.com>
2025-03-21 07:12:42 +0700

    init

---
+++ API/KtraGiuaKyAPI/src/main/java/com/giuaky/ktragiuakyapi/KtraGiuaKyApiApplication.java
@@ -0,0 +1,13 @@
+package com.giuaky.ktragiuakyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtraGiuaKyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtraGiuaKyApiApplication.class, args);
+    }
+
+}
---
+++ API/KtraGiuaKyAPI/src/test/java/com/giuaky/ktragiuakyapi/KtraGiuaKyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giuaky.ktragiuakyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtraGiuaKyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
