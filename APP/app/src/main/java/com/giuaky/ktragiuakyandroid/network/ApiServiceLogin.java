71ab4bde86714a02a2a07bb559ebaf09c4478ee6
Pham Trung Ky <phamtrungky@gmail.com>
2025-03-21 09:22:34 +0700

    Login_TrungKy

--- APP/app/src/main/java/com/giuaky/ktragiuakyandroid/network/ApiServiceLogin.java
+++ APP/app/src/main/java/com/giuaky/ktragiuakyandroid/network/ApiServiceLogin.java
@@ -3,10 +3,12 @@ package com.giuaky.ktragiuakyandroid.network;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface ApiServiceLogin {
 
-    @GET("categories.php")
-    Call<List<LoginRequest>> getCategoryAll();
+    @POST
+    Call<LoginRequest> login(@Body LoginRequest loginRequest);
 }
