b00bc6e7601306728f8f551e339a3b1e6ee7442d
loidz <loideptrai114@gmail.com>
2025-03-21 08:15:07 +0700

    first commit

---
+++ API/KtraGiuaKyAPI/src/main/java/com/giuaky/ktragiuakyapi/UserCreateRequest.java
@@ -0,0 +1,22 @@
+package com.giuaky.ktragiuakyapi;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class UserCreateRequest {
+	@NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    private String avatarUrl;
+}
