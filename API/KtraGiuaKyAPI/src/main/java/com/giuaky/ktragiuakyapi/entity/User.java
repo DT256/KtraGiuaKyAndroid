c554f1d2942d162e8248cc61ceeec5ac7b178fca
dt256 <buiducthang13022004@gmail.com>
2025-03-21 10:26:32 +0700

    update

--- API/KtraGiuaKyAPI/src/main/java/com/giuaky/ktragiuakyapi/entity/User.java
+++ API/KtraGiuaKyAPI/src/main/java/com/giuaky/ktragiuakyapi/entity/User.java
@@ -30,4 +30,6 @@ public class User {
 
     private String avatarUrl;
 
+    private boolean isActive;
+
 }
--- API/KtraGiuaKyAPI/src/main/java/com/giuaky/ktragiuakyapi/services/Impl/AuthServiceImpl.java
+++ API/KtraGiuaKyAPI/src/main/java/com/giuaky/ktragiuakyapi/services/Impl/AuthServiceImpl.java
@@ -16,21 +16,19 @@ public class AuthServiceImpl implements IAuthService {
     @Autowired
     private UserRepository userRepository;
 
+    //Tran Phi Thang 22110424
     @Override
     public String register(UserCreateRequest request) {
         Optional<User> existingUser = userRepository.findByEmail(request.getEmail());
-
         if (existingUser.isPresent()) {
             throw new RuntimeException("Email already registered");
         }
-
         User newUser = new User();
         newUser.setEmail(request.getEmail());
         newUser.setPassword(request.getPassword());
-
-
+        newUser.setActive(false);
+        newUser.setUsername(request.getUsername());
         userRepository.save(newUser);
-
         return "User created";
     }
 
@@ -38,8 +36,10 @@ public class AuthServiceImpl implements IAuthService {
     public UserResponse login(UserLoginRequest request) {
         User user = userRepository.findByUsername(request.getUsername())
                 .orElseThrow(() -> new RuntimeException("Invalid email or password"));
-
-        if (request.getPassword()==user.getPassword()) {
+        if(!user.isActive()) {
+            throw new RuntimeException("User is not active");
+        }
+        if (!user.getPassword().equals(request.getPassword())) {
             throw new RuntimeException("Invalid email or password");
         }
         UserResponse  userResponse = new UserResponse();
