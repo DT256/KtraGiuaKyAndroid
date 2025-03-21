969e6e315a1ebd1f2baa0b63e0b6553f0bdec5e5
HoangPhuc <phucnguyeho@gmail.com>
2025-03-21 09:47:16 +0700

    otp UI call APIs

---
+++ APP/app/src/main/java/com/giuaky/ktragiuakyandroid/dto/OtpRequest.java
@@ -0,0 +1,16 @@
+package com.giuaky.ktragiuakyandroid.dto;
+
+/**
+ * Author: 22110400 - Nguyen Hoang Phuc
+ */
+public class OtpRequest {
+    private String email;
+
+    public OtpRequest(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
