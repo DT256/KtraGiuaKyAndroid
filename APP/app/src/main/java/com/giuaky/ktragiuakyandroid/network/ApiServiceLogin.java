50d68c35dad6526bc2b0c4db4c2237afdc426408
Pham Trung Ky <phamtrungky@gmail.com>
2025-03-21 09:30:57 +0700

    .

--- APP/app/src/main/java/com/giuaky/ktragiuakyandroid/network/ApiServiceLogin.java
+++ APP/app/src/main/java/com/giuaky/ktragiuakyandroid/network/ApiServiceLogin.java
@@ -3,12 +3,13 @@ package com.giuaky.ktragiuakyandroid.network;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiServiceLogin {
 
-    @POST
-    Call<LoginRequest> login(@Body LoginRequest loginRequest);
+    @POST("login")
+    Call<Response<LoginRequest>> login(@Body LoginRequest loginRequest);
 }
